238586b9607bf8ab551f0ad6b5c314a42d607b2c
Your Name <paragpawar0077@gmail.com>
2026-05-16 16:15:17 +0530

    extracted audio from video  and extracted text from audio

---
+++ process_videos.py
@@ -0,0 +1,30 @@
+#Step 1 of the RAG Pipeline
+# COnverts the video to mp3
+import os
+import subprocess
+
+files = os.listdir('videos')
+
+# used enumerate with zfill(2) so the pipeline scales automatically to any number of videos without any code changes.
+for index, file in enumerate(files, start=1):
+    file_name = os.path.splitext(file)[0]        # Clean name without .mp4
+    tutorial_number = str(index).zfill(2)        # Auto: 01, 02, 03
+
+
+    os.makedirs("audios", exist_ok=True)
+
+    print(tutorial_number, file_name)
+
+    # subprocess.run() executes a terminal command from Python.
+    # This is equivalent to running in terminal:
+    #   ffmpeg -i "videos/filename.mp4" -q:a 0 -map a "audios/02_Title.mp3"
+
+    subprocess.run([
+        "ffmpeg",                       #→ the tool we're calling
+        "-i", f"videos/{file}",         # → input flag; next arg is the input file,full path to the input video file
+        "-q:a", "0",                    # → best audio quality (0 = highest, 9 = lowest)
+        "-map", "a",                    #  → extract ONLY the audio stream (ignore video)
+        f"audios/{tutorial_number}_{file_name}.mp3" # → output path; saves as numbered .mp3
+                                                    #e.g. "audios/02_Your First HTML Website.mp3"
+
+    ])
---
+++ translate.py
@@ -0,0 +1,29 @@
+import os
+import whisper
+import json
+
+# Load Whisper model (small = faster, good enough for demo)
+model = whisper.load_model("small")
+
+# Get all .mp3 files from audios folder
+audio_files = os.listdir("audios")
+
+# Loop through each audio file
+for audio_file in audio_files:
+
+    audio_path = os.path.join("audios", audio_file)
+
+    print(f"Transcribing: {audio_file}...")
+
+    result = model.transcribe(audio_path, language="hi", task="translate")
+
+    # Save the transcript as a JSON file in jsons/ folder
+    # so chunks.py can pick it up in the next step
+    os.makedirs("jsons", exist_ok=True)
+
+    output_path = os.path.join("jsons", f"{os.path.splitext(audio_file)[0]}.json")
+
+    with open(output_path, "w") as f:
+        json.dump(result, f, indent=2)
+
+    print(f"Saved → {output_path}")
